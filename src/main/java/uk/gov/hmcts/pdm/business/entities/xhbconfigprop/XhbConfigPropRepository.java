901b37b1a35b294ff115ab66eab4d95af6f8c2ff
Luke Gittins <luke.gittins@hmcts.net>
2025-07-18 17:01:32 +0100

    PDDA-532 Fixing sonar vulnerability

--- src/main/java/uk/gov/hmcts/pdm/business/entities/xhbconfigprop/XhbConfigPropRepository.java
+++ src/main/java/uk/gov/hmcts/pdm/business/entities/xhbconfigprop/XhbConfigPropRepository.java
@@ -3,19 +3,13 @@ package uk.gov.hmcts.pdm.business.entities.xhbconfigprop;
 import com.pdm.hb.jpa.EntityManagerUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.pdm.business.entities.AbstractRepository;
 
-import java.io.Serializable;
 import java.util.List;
 
 @Repository
-public class XhbConfigPropRepository extends AbstractRepository<XhbConfigPropDao> implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-    private static final Logger LOG = LoggerFactory.getLogger(XhbConfigPropRepository.class);
+public class XhbConfigPropRepository extends AbstractRepository<XhbConfigPropDao> {
 
     public XhbConfigPropRepository(EntityManager em) {
         super(em);
@@ -27,7 +21,7 @@ public class XhbConfigPropRepository extends AbstractRepository<XhbConfigPropDao
     }
 
     /**
-     * findByPropertyName.
+     * findByPropertyNameSafe.
      * @param propertyName String
      * @return List
      */
