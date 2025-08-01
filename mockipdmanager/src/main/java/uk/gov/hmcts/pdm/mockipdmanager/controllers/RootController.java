50d49d44a115535b9baebdbafe8c1ab44ecc6b29
Scott Atwell <scott.atwell@cgi.com>
2025-08-01 14:23:09 +0100

    Upgraded checkstyle and related changes

--- mockipdmanager/src/main/java/uk/gov/hmcts/pdm/mockipdmanager/controllers/RootController.java
+++ mockipdmanager/src/main/java/uk/gov/hmcts/pdm/mockipdmanager/controllers/RootController.java
@@ -15,7 +15,7 @@ public class RootController {
     /**
      * Root GET endpoint.
      *
-     * <p>Azure application service has a hidden feature of making requests to root endpoint when
+     * Azure application service has a hidden feature of making requests to root endpoint when
      * "Always On" is turned on.
      * This is the endpoint to deal with that and therefore silence the unnecessary 404s as a response code.
      *
